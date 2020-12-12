6c2c4a915eb8daddb7a082bf0da58b645ad4a179
devale <3052927+devale@users.noreply.github.com>
2020-12-13 00:00:39 +0100

    day11 complete!

--- day11/day11.py
+++ day11/day11.py
@@ -32,7 +32,7 @@ def char_to_int(l):
     #log(f'converting {l} to {res}')
     return res
 
-def check_surrounding_cells(d, pos, tollerance=3):
+def check_surrounding_cells(d, pos, tolerance=3):
     '''checks 8 surrounding seats (until tollerance) of position pos (x,y) and returns occupancy in next round  (True = seated, False = empty)'''
     taken = 0
     y, x = pos[0], pos[1]
@@ -55,70 +55,98 @@ def check_surrounding_cells(d, pos, tollerance=3):
             else:
                 #log(f'taken: yx-check {y_check},{x_check} has val: {d[y_check][x_check]}')
                 taken += 1
-                if taken > tollerance:
+                if taken > tolerance:
                     return taken
     return taken
 
 
 
-def check_sightline_cells(d, pos, tollerance=4):
+def check_sightline_cells(d, pos, tolerance=4):
     '''count taken seats in sightlines (until tollerance) of position pos (x,y) and returns occupancy in next round  (True = seated, False = empty)'''
     taken = 0
     y, x = pos[0], pos[1]
     max_x = len(d[0]) - 1
     max_y = len(d) - 1
     for sign in [-1,1]: 
-        for ydelta in range(1,max_y): # up then down
+        log(f'sign: {sign}')
+        for ydelta in range(1,max_y): # up then down depending on sign
             y_check = y + ydelta * sign
-            log(f'for y:{y}, checking y:{y_check}')
-            if 0 > y_check > max_y:
+            log(f'verticals: for y,x:{y},{x} checking y_check:{y_check}')
+            if y_check < 0 or y_check > max_y:
+                log('out of bounds')
                 break
             seat = d[y_check][x]
+            log(f'seat: {seat}')
             if seat == -1: #nonseat, keep looking
+                log('nonseat')
                 continue
             elif seat == 1: # taken
+                log('taken')
                 taken += 1
-                if taken > tollerance:
+                if taken > tolerance:
                     return taken
+            log(f'empty')
             break #else empty seat, stop looking
         
-        for xdelta in range(1,max_x): # left then right
+        for xdelta in range(1,max_x): # left then right depending on sign
             x_check = x + xdelta * sign
-            log(f'for x:{x}, checking x:{x_check}')
-            if 0 > x_check > max_x:
+            log(f'horizontals: for y,x:{y},{x}, checking x:{x_check}')
+            if x_check < 0 or x_check > max_x:
                 break
             seat = d[y][x_check]
             if seat == -1: #nonseat, keep looking
                 continue
             elif seat == 1: # taken
                 taken += 1
-                if taken > tollerance:
+                if taken > tolerance:
                     return taken
             break #else empty seat, stop looking
-
-        for 
-
-
-
-        x_check1,x_check2 = x+xdelta, x-xdelta 
-        log(f'for yx {y} {x}, checking y:{y_check} and x:{x_check}')
-        #skip when comparing itself, or out of matrix bounds
         
-        if ((xdelta == 0 and ydelta == 0 )  
-            or x_check < 0 or x_check > max_x 
-            or y_check < 0 or y_check > max_y): 
-            #log(f'out of bounds or self')
-            continue
-        elif d[y_check][x_check] != 1:
-            #log('not taken')
-            continue
-        else:
-            #log(f'taken: yx-check {y_check},{x_check} has val: {d[y_check][x_check]}')
-            taken += 1
-            if taken > tollerance: 
-                log(f'found >{tollerance} taken seats' )
-                return taken
+
+        max_len = max(max_x, max_y)
+        #positive diagonals
+        for delta in range(1,max_len): 
+            x_check = x + delta * sign
+            y_check = y + delta * sign
+            if (x_check < 0 or x_check > max_x
+                or y_check < 0 or y_check > max_y): #out of bounds
+                break
+            log(f'positive diagonals: for y,x:{y},{x}, checking y,x:{y_check},{x_check}')
+            if  y_check < 0 or y_check > max_y: #out of bounds
+                break
+            seat = d[y_check][x_check]
+            log(f'seat = {seat}')
+            if seat == -1:
+                continue
+            elif seat == 1:
+                taken += 1
+                if taken > tolerance:
+                    return taken
+            break #seat empty, stop looking. 
+
+        #negative diagonals
+        for delta in range(1,max_len): 
+            x_check = x + delta * sign
+            y_check = y + delta * sign * -1 #negative
+            if (x_check < 0 or x_check > max_x
+                or y_check < 0 or y_check > max_y): #out of bounds
+                break
+            log(f'negative diagonals: for y,x:{y},{x}, checking y,x:{y_check},{x_check}')
+            if  y_check < 0 or y_check > max_y: #out of bounds
+                break
+            seat = d[y_check][x_check]
+            log(f'seat = {seat}')
+            if seat == -1:
+                continue
+            elif seat == 1:
+                taken += 1
+                if taken > tolerance:
+                    return taken
+            break #seat empty, stop looking. 
+
     return taken
+
+
 
 
 
@@ -132,10 +160,23 @@ d = [ \
 
 print(check_surrounding_cells(d, [1,0]) ) #returns True
 print(check_surrounding_cells(d, [1,2]) ) #returns False
+
+LOGGING = 1
+#part 2
+d = [ \
+[ 1,-1, 1,-1, 1],
+[-1,-1,-1,-1,-1],
+[ 1,-1, 0,-1, 1],
+[-1,-1,-1,-1,-1],
+[ 1, 1, 1, 1, 1]] #2 taken
+
+#print(check_sightline_cells(d, [2,2]) ) #returns 5
+#print(check_sightline_cells(d, [3,2]) ) #returns 3
+
 '''
 
-def update_iteration(d):
-    '''for seat in seats, check_surrounding seats. create a matrix of 0 and 1s. take a copy to loop through, and pass it to other func?? '''
+def update_iteration(d, tolerance=3, part=1):
+    '''take a copy to loop through, for seat in seats pass it to other func check_surrounding seats. '''
     d_new = copy.deepcopy(d)
     #log(f'd_new: {d_new} ' )
     updates = 0
@@ -145,12 +186,16 @@ def update_iteration(d):
             if val == -1 : continue
             else:
                 #log(f'checking y-x-pos {y_pos},{x_pos} for value {val}')
-                cnt = check_surrounding_cells(d, (y_pos, x_pos))
+                if part==1:
+                    cnt = check_surrounding_cells(d, (y_pos, x_pos), tolerance)
+                elif part==2:
+                    cnt = check_sightline_cells(d, (y_pos, x_pos), tolerance)
+
                 if val == 0 and cnt == 0: #take seat
                     #log(f'taking seat')
                     d_new[y_pos][x_pos] = 1
                     updates += 1
-                elif val == 1 and cnt == 4: #vacant seat
+                elif val == 1 and cnt > tolerance: #vacant seat
                     #log(f'leaving seat')
                     d_new[y_pos][x_pos] = 0 
                     updates += 1
@@ -168,7 +213,18 @@ def solve1(d):
     updates = 1
     while updates > 0 : #while not stable + safety
         iterate_cnt += 1
-        d, updates = update_iteration(d)
+        d, updates = update_iteration(d, tolerance=3, part=1)
+        log(f'iteration {iterate_cnt} did {updates} updates')
+    
+    return sum([i.count(1) for i in d])
+
+def solve2(d):
+    '''run updates until stable'''
+    iterate_cnt = 0
+    updates = 1
+    while updates > 0 : #while not stable + safety
+        iterate_cnt += 1
+        d, updates = update_iteration(d, tolerance=4, part=2)
         log(f'iteration {iterate_cnt} did {updates} updates')
     
     return sum([i.count(1) for i in d])
@@ -184,16 +240,16 @@ data = list(map(char_to_int, open(f_loc, 'r').readlines()))
 
 #part 1: run iterations until seating is stable
 #log(data)
-print(f'part 1: {solve1(data)}')
+#print(f'part 1: {solve1(data)}')
 # test: 37 occupied seats
 # real: 2489
 
 #part 2: how many combinations can be made with the adapters to connect the charging outlet to your device? 
-#print(f'part 2: {solve2(d)}')
-# test:  
-# real: 
+#print(f'part 2: {solve2(data)}')
+# test: 26 
+# real: 2180
 
 # timeit
-#print(f'timeit: {timefunc(10, solve1, data)}' )          
+print(f'timeit: {timefunc(3, solve2, data)}' )          
 # part 1: 9.5s
-# part 2:
+# part 2: 41s
